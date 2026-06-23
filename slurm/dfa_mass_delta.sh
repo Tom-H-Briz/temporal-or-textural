b4b27718a0f7d579c09a093bfbc7dbf6eca2237d
Tom-H-Briz <gq25877@bristol.ac.uk>
2026-06-23 11:37:11 +0100

    updating for missing apptainer

---
+++ slurm/dfa_mass_delta.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=dfa_mass_delta
+#SBATCH --output=dfa_mass_delta_%j.out
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --time=01:00:00
+
+source $HOME/.tokens
+
+export VIDEO_DIR="/scratch/b5bg/tomheslin83.b5bg/videos"
+export LABELS_PATH="$HOME/labels/labels.json"
+export VALIDATION_PATH="$HOME/labels/validation.json"
+
+SIF="$SCRATCHDIR/pytorch_25.05-py3.sif"
+
+apptainer exec --nv \
+    --bind $HOME:$HOME \
+    --bind $SCRATCHDIR:$SCRATCHDIR \
+    $SIF \
+    bash -c "
+        pip install --quiet av einops pandas pyarrow matplotlib transformers huggingface-hub tqdm &&
+        cd $HOME/temporal-or-textural &&
+        python src/stage3_analysis/dfa_mass_delta.py
+    "
